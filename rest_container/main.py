00faae1d370d1bb0aa538952ef45f0f2554deccd
Matheus Castello <matheus@castello.eng.br>
2019-08-20 02:26:49 -0300

    rest: container: Add cors for flask
    Quasar UI will made HTTP request from another ip and port so
    we need enable cors on our rest API
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- rest_container/main.py
+++ rest_container/main.py
@@ -7,6 +7,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 from deviceInfo import DeviceInfo
 
 # global sensor
@@ -14,37 +15,51 @@ deviceInfo = DeviceInfo()
 
 # flask route
 app = Flask(__name__)
+# add cors
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
-# GET requests for CPU 
+# GET requests for CPU
 @app.route('/cpu')
+@cross_origin()
 def cpu_info():
-	ret = {'cores' : deviceInfo.getCPUCoresCount(), 'temperatures' : [], 'usage': 0}
-	ret['temperatures'] = { 'A53': deviceInfo.getTemperatureCPUA53(), 'A72': deviceInfo.getTemperatureCPUA72() }
-	ret['usage'] = deviceInfo.getCPUUsage()
-	return jsonify(ret)
+    ret = {'cores': deviceInfo.getCPUCoresCount(), 'temperatures': [],
+           'usage': 0}
+    ret['temperatures'] = {'A53': deviceInfo.getTemperatureCPUA53(
+    ), 'A72': deviceInfo.getTemperatureCPUA72()}
+    ret['usage'] = deviceInfo.getCPUUsage()
+    return jsonify(ret)
 
 # GET requests for GPU
 @app.route('/gpu')
+@cross_origin()
 def gpu_info():
-	ret = {'cores' : 2, 'temperatures' : [], 'memoryUsage' : deviceInfo.getGPUMemoryUsage()}
-	ret['temperatures'] = { 'GPU0': deviceInfo.getTemperatureGPU0(), 'GPU1': deviceInfo.getTemperatureGPU1() }
-	return jsonify(ret)
+    ret = {'cores': 2, 'temperatures': [],
+           'memoryUsage': deviceInfo.getGPUMemoryUsage()}
+    ret['temperatures'] = {'GPU0': deviceInfo.getTemperatureGPU0(
+    ), 'GPU1': deviceInfo.getTemperatureGPU1()}
+    return jsonify(ret)
 
 # GET requests for GPU
 @app.route('/ram')
+@cross_origin()
 def ram_info():
-	ret = {'total' : deviceInfo.getRAMTotal(), 'usage' : deviceInfo.getRAMUsage(), 'free' : deviceInfo.getRAMFree()}
-	return jsonify(ret)
+    ret = {'total': deviceInfo.getRAMTotal(), 'usage': deviceInfo.getRAMUsage(),
+           'free': deviceInfo.getRAMFree()}
+    return jsonify(ret)
+
 
 if __name__ == '__main__':
-	print("Flask working")
-	app.run(host='0.0.0.0', port='5001')
+    print("Flask working well")
+    app.run(host='0.0.0.0', port='5001')
 else:
-	print(__name__)
-	print("Flask not working")
+    print(__name__)
+    print("Flask not working")
 
 # This is a dummy handler and will not be invoked
 # Instead the code above will be executed in an infinite loop for our example
+
+
 def function_handler(event, context):
-	return
+    return
