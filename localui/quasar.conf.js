fa4d27ad6b950d05b520d4f6468338f09145947f
Matheus Castello <matheus@castello.eng.br>
2019-08-20 13:33:48 -0300

    localui: Add aarch64 as package build for electron
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- localui/quasar.conf.js
+++ localui/quasar.conf.js
@@ -155,17 +155,22 @@ module.exports = function (ctx) {
       },
 
       packager: {
-        // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
-        // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
-
-        // Windows only
-        // win32metadata: { ... }
+        platform: 'linux',
+        arch: 'arm64'
       },
+
+      // packager: {
+      // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
+
+      // OS X / Mac App Store
+      // appBundleId: '',
+      // appCategoryType: '',
+      // osxSign: '',
+      // protocol: 'myapp://path',
+
+      // Windows only
+      // win32metadata: { ... }
+      // },
 
       builder: {
         // https://www.electron.build/configuration/configuration
