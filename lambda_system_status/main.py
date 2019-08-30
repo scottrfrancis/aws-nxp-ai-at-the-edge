4012c6b4a0e638d3d09ed0feb9b6fe811df85c55
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-08-30 08:36:45 -0300

    lambda_system_status: add MQTT topics
    Add topics for camera, conveyor belt and led.

--- lambda_system_status/main.py
+++ lambda_system_status/main.py
@@ -14,6 +14,17 @@ import threading
 import time
 import json
 
+system_data_port = "5001"
+conveyor_belt_port = "5002"
+rest = {
+	"cpu/data": "http://localhost:" + system_data_port + "/cpu",
+	"gpu/data": "http://localhost:" + system_data_port + "/gpu",
+	"ram/data": "http://localhost:" + system_data_port + "/ram",
+	"cam/data": "http://localhost:" + system_data_port + "/cam",
+	"cb/data": "http://localhost:" + conveyor_belt_port + "/cb",
+	"led/data": "http://localhost:" + conveyor_belt_port + "/led"
+}
+
 # Creating a greengrass core sdk client
 client = greengrasssdk.client('iot-data')
 # Retrieving platform information to send from Greengrass Core
@@ -24,29 +35,56 @@ def greengrass_mqtt_run():
 	while True:
 		# cpu/data
 		try:
-			cpuData = requests.get("http://localhost:5001/cpu")
+			cpuData = requests.get(rest["cpu/data"])
 			client.publish(topic='cpu/data', payload=cpuData)
 			print("Mqtt cpu/data published ...")
 		except requests.exceptions.ConnectionError:
-			print("Connection error, retry on next loop!")
+			print("Connection error: cpu/data, retry on next loop!")
 		except Exception as e:
 			print("Unknown exception: " + repr(e))
 		# gpu/data
 		try:
-			gpuData = requests.get("http://localhost:5001/gpu")
+			gpuData = requests.get(rest["gpu/data"])
 			client.publish(topic='gpu/data', payload=gpuData)
 			print("Mqtt gpu/data published ...")
 		except requests.exceptions.ConnectionError:
-			print("Connection error, retry on next loop!")
+			print("Connection error: gpu/data, retry on next loop!")
 		except Exception as e:
 			print("Unknown exception: " + repr(e))
 		# ram/data
 		try:
-			ramData = requests.get("http://localhost:5001/ram")
+			ramData = requests.get(rest["ram/data"])
 			client.publish(topic='ram/data', payload=ramData)
 			print("Mqtt ram/data published ...")
 		except requests.exceptions.ConnectionError:
-			print("Connection error, retry on next loop!")
+			print("Connection error: ram/data, retry on next loop!")
+		except Exception as e:
+			print("Unknown exception: " + repr(e))
+		# cam/data
+		try:
+			ramData = requests.get(rest["cam/data"])
+			client.publish(topic='cam/data', payload=ramData)
+			print("Mqtt cam/data published ...")
+		except requests.exceptions.ConnectionError:
+			print("Connection error: cam/data, retry on next loop!")
+		except Exception as e:
+			print("Unknown exception: " + repr(e))
+		# cb/data
+		try:
+			ramData = requests.get(rest["cb/data"])
+			client.publish(topic='cb/data', payload=ramData)
+			print("Mqtt cb/data published ...")
+		except requests.exceptions.ConnectionError:
+			print("Connection error: cb/data, retry on next loop!")
+		except Exception as e:
+			print("Unknown exception: " + repr(e))
+		# led/data
+		try:
+			ramData = requests.get(rest["led/data"])
+			client.publish(topic='led/data', payload=ramData)
+			print("Mqtt led/data published ...")
+		except requests.exceptions.ConnectionError:
+			print("Connection error: led/data, retry on next loop!")
 		except Exception as e:
 			print("Unknown exception: " + repr(e))
 
