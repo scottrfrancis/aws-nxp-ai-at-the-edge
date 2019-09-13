15c787ef4025201fba867baec75c3cc4b358c281
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-09-13 15:31:24 -0300

    container_system_status: build.sh: choose to build prod or dev

--- container_system_status/build.sh
+++ container_system_status/build.sh
@@ -3,10 +3,31 @@
 DOCKER_COMPOSE_DIR=./docker_compose
 cd $DOCKER_COMPOSE_DIR
 
-for fname in $(ls docker-compose*.build.yml)
+BUILD_TYPE=("prod" "dev/next")
+
+PS3="Select a build type: "
+select opt in "${BUILD_TYPE[@]}"
 do
-    # Build for all arch
-    docker-compose -f $fname build
-    # Push for all arch
-    docker-compose -f $fname push
+    if [[ " ${BUILD_TYPE[@]} " =~ " ${opt} " ]]; then
+        if [[ "${opt}" == "prod" ]]; then
+            for fname in $(ls docker-compose*.build.yml)
+            do
+                # Build for all arch
+                docker-compose -f $fname build
+                # Push for all arch
+                docker-compose -f $fname push
+            done
+        elif [[ "${opt}" == "dev/next" ]]; then
+            for fname in $(ls docker-compose*.build-next.yml)
+            do
+                # Build for all arch
+                docker-compose -f $fname build
+                # Push for all arch
+                docker-compose -f $fname push
+            done
+        fi
+        break
+    else
+        echo "Build type not supported"
+    fi
 done
