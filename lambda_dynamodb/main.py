ff9e119319cba1b6dd349efd3cc1b316357a8d3b
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-09-13 16:56:27 -0300

    lambda_dynamodb: write to DB using batch operation
    This makes it more reliable and faster.

--- lambda_dynamodb/main.py
+++ lambda_dynamodb/main.py
@@ -72,133 +72,126 @@ def function_handler(event, context):
 
 	# Record full system snapshot to DynamoDB
 	global tableName
-	table = dynamodb.Table(tableName)
 
-	# CPU
 
+	# Calculate timestamps for each piece of data
 	cpudatetime = datetime.utcfromtimestamp(int(event["current"]["metadata"]["reported"]["cpu"]["temperatures"]["A53"]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
-	res = table.put_item(
-		Item={
-			'pk' : cpudatetime.split()[0],
-			'sk' : cpudatetime.split()[1] + '-cpu',
-			#'data' : {
-			#	'cpu' : {
-			#		'a53-temperature': Decimal(str(event["current"]["state"]["reported"]["cpu"]["temperatures"]["A53"])),
-			#		'a72-temperature': Decimal(str(event["current"]["state"]["reported"]["cpu"]["temperatures"]["A72"])),
-			#		'processing-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usage"]))
-			#	}
-			#},
-			'system': 'cpu',
-			'a53-temperature': Decimal(str(event["current"]["state"]["reported"]["cpu"]["temperatures"]["A53"])),
-			'a72-temperature': Decimal(str(event["current"]["state"]["reported"]["cpu"]["temperatures"]["A72"])),
-			'processing-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usage"]))
-		}
-	)
-	print("Added CPU item: " + str(res))
-
-	# GPU
-
 	gpudatetime = datetime.utcfromtimestamp(int(event["current"]["metadata"]["reported"]["gpu"]["temperatures"]["GPU0"]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
-	res = table.put_item(
-		Item={
-			'pk' : gpudatetime.split()[0],
-			'sk' : gpudatetime.split()[1] + '-gpu',
-			#'data' : {
-			#	'gpu' : {
-			#		'gpu0-temperature' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["temperatures"]["GPU0"])),
-			#		'gpu1-temperature' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["temperatures"]["GPU1"])),
-			#		'memory-load' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["memoryUsage"]))
-			#	}
-			#},
-			'system': 'gpu',
-			'gpu0-temperature' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["temperatures"]["GPU0"])),
-			'gpu1-temperature' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["temperatures"]["GPU1"])),
-			'memory-load' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["memoryUsage"]))
-		}
-	)
-	print("Added GPU item: " + str(res))
-
-	# RAM
-
 	ramdatetime = datetime.utcfromtimestamp(int(event["current"]["metadata"]["reported"]["ram"]["free"]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
-	res = table.put_item(
-	Item={
-			'pk' : ramdatetime.split()[0],
-			'sk' : ramdatetime.split()[1] + '-ram',
-			#'data' : {
-			#	'ram' : {
-			#		'memory-free' : Decimal(str(event["current"]["state"]["reported"]["ram"]["free"])),
-			#		'memory-load' : Decimal(str(event["current"]["state"]["reported"]["ram"]["usage"]))
-			#	}
-			#},
-			'system': 'ram',
-			'memory-free' : Decimal(str(event["current"]["state"]["reported"]["ram"]["free"])),
-			'memory-load' : Decimal(str(event["current"]["state"]["reported"]["ram"]["usage"]))
-		}
-	)
-	print("Added RAM item: " + str(res))
-
-	# Inference
-
 	inferencedatetime = datetime.utcfromtimestamp(int(event["current"]["metadata"]["reported"]["ram"]["free"]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
-	res = table.put_item(
-	Item={
-			'pk' : inferencedatetime.split()[0],
-			'sk' : inferencedatetime.split()[1] + '-inference',
-			#'data' : {
-			#	'inference' : {
-			#		'pasta-type' : Decimal(str(event["current"]["state"]["reported"]["inference"]["pasta-type"])),
-			#		'confidence' : Decimal(str(event["current"]["state"]["reported"]["inference"]["confidence"])),
-			#		'inference-time' : Decimal(str(event["current"]["state"]["reported"]["inference"]["inference-time"]))
-			#	}
-			#},
-			'system': 'inference',
-			'pasta-type' : Decimal(randrange(0, 4)),
-			'confidence' : Decimal(str(uniform(0.0, 100.0))),
-			'inference-time' : Decimal(randrange(75, 483))
-			#'pasta-type' : Decimal(str(event["current"]["state"]["reported"]["inference"]["pasta-type"])),
-			#'confidence' : Decimal(str(event["current"]["state"]["reported"]["inference"]["confidence"])),
-			#'inference-time' : Decimal(str(event["current"]["state"]["reported"]["inference"]["inference-time"]))
-		}
-	)
-	print("Added inference item: " + str(res))
-
-	# Conveyor Belt
-
 	cbdatetime = datetime.utcfromtimestamp(int(event["current"]["metadata"]["reported"]["ram"]["free"]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
-	res = table.put_item(
-	Item={
-			'pk' : cbdatetime.split()[0],
-			'sk' : cbdatetime.split()[1] + '-cb',
-			#'data' : {
-			#	'cb' : {
-			#		'speed' : Decimal(str(event["current"]["state"]["reported"]["cb"]["speed"]))
-			#	}
-			#},
-			'system': 'cb',
-			'speed' : Decimal(randrange(46, 63, 3))
-			#'speed' : Decimal(str(event["current"]["state"]["reported"]["cb"]["speed"]))
-		}
-	)
-	print("Added conveyor belt item: " + str(res))
-
-	# LED
-
 	leddatetime = datetime.utcfromtimestamp(int(event["current"]["metadata"]["reported"]["ram"]["free"]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
-	res = table.put_item(
-	Item={
-			'pk' : leddatetime.split()[0],
-			'sk' : leddatetime.split()[1] + '-led',
-			#'data' : {
-			#	'led' : {
-			#		'brightness' : Decimal(str(event["current"]["state"]["reported"]["led"]["brightness"]))
-			#	}
-			#},
-			'system': 'led',
-			'brightness' : Decimal(randrange(87, 93, 1))
-			#'brightness' : Decimal(str(event["current"]["state"]["reported"]["led"]["brightness"]))
+	infodatetime = datetime.utcfromtimestamp(int(event["current"]["metadata"]["reported"]["info"]["board-serial"]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
+
+	boardSerial = event["current"]["state"]["reported"]["info"]["board-serial"]
+
+	res = dynamodb.batch_write_item(
+		RequestItems={
+			tableName : [
+				{
+					'PutRequest' : {
+						'Item': {
+							'pk' : cpudatetime.split()[0],
+							'sk' : cpudatetime.split()[1] + '-cpu-' + str(boardSerial),
+							'system': 'cpu',
+							'a53-temperature': Decimal(str(event["current"]["state"]["reported"]["cpu"]["temperatures"]["A53"])),
+							'a72-temperature': Decimal(str(event["current"]["state"]["reported"]["cpu"]["temperatures"]["A72"])),
+							'processing-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usage"])),
+							'a53-0-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usageDetailed"]["A53-0"])),
+							'a53-1-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usageDetailed"]["A53-1"])),
+							'a53-2-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usageDetailed"]["A53-2"])),
+							'a53-3-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usageDetailed"]["A53-3"])),
+							'a72-0-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usageDetailed"]["A72-0"])),
+							'a72-1-load': Decimal(str(event["current"]["state"]["reported"]["cpu"]["usageDetailed"]["A72-1"]))
+						}
+					}
+				},
+				{
+					'PutRequest' :{
+						'Item': {
+							'pk' : gpudatetime.split()[0],
+							'sk' : gpudatetime.split()[1] + '-gpu-' + str(boardSerial),
+							'system': 'gpu',
+							'gpu0-temperature' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["temperatures"]["GPU0"])),
+							'gpu1-temperature' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["temperatures"]["GPU1"])),
+							'memory-load' : Decimal(str(event["current"]["state"]["reported"]["gpu"]["memoryUsage"]))
+						}
+					}
+				},
+				{
+					'PutRequest' :{
+						'Item': {
+							'pk' : ramdatetime.split()[0],
+							'sk' : ramdatetime.split()[1] + '-ram-' + str(boardSerial),
+							'system': 'ram',
+							'memory-free' : Decimal(str(event["current"]["state"]["reported"]["ram"]["free"])),
+							'memory-load' : Decimal(str(event["current"]["state"]["reported"]["ram"]["usage"]))
+						}
+					}
+				},
+				{
+					'PutRequest' :{
+						'Item': {
+							'pk' : inferencedatetime.split()[0],
+							'sk' : inferencedatetime.split()[1] + '-inference-' + str(boardSerial),
+							'system': 'inference',
+							'pasta-type' : Decimal(randrange(0, 4)),
+							'confidence' : Decimal(str(uniform(0.0, 100.0))),
+							'inference-time' : Decimal(randrange(75, 483))
+							#'pasta-type' : Decimal(str(event["current"]["state"]["reported"]["inference"]["pasta-type"])),
+							#'confidence' : Decimal(str(event["current"]["state"]["reported"]["inference"]["confidence"])),
+							#'inference-time' : Decimal(str(event["current"]["state"]["reported"]["inference"]["inference-time"]))
+						}
+					}
+				},
+				{
+					'PutRequest' :{
+						'Item': {
+							'pk' : cbdatetime.split()[0],
+							'sk' : cbdatetime.split()[1] + '-cb-' + str(boardSerial),
+							'system': 'cb',
+							'speed' : Decimal(str(event["current"]["state"]["reported"]["cb"]["speed"]))
+						}
+					}
+				},
+				{
+					'PutRequest' :{
+						'Item': {
+							'pk' : leddatetime.split()[0],
+							'sk' : leddatetime.split()[1] + '-led-' + str(boardSerial),
+							'system': 'led',
+							'brightness' : Decimal(str(event["current"]["state"]["reported"]["led"]["brightness"]))
+						}
+					}
+				},
+				#{
+				#	'PutRequest' :{
+				#		'Item': {
+				#			'pk' : leddatetime.split()[0],
+				#			'sk' : leddatetime.split()[1] + '-info-' + str(boardSerial),
+				#			'system': 'info',
+				#			'board-revision' : event["current"]["state"]["reported"]["info"]["board-revision"],
+				#			'board-serial': event["current"]["state"]["reported"]["info"]["board-serial"],
+				#			'board-type': event["current"]["state"]["reported"]["info"]["board-type"]
+				#		}
+				#	}
+				#}
+				{
+					'PutRequest' :{
+						'Item': {
+							'pk' : 'board-detail',
+							'sk' : str(boardSerial),
+							'board-revision' : event["current"]["state"]["reported"]["info"]["board-revision"],
+							'board-type': event["current"]["state"]["reported"]["info"]["board-type"],
+							'last-update-time': infodatetime
+						}
+					}
+				}
+			]
 		}
 	)
-	print("Added LED item: " + str(res))
+
+	logger.info("Response from DynaomDB request: ")
+	logger.info(res)
 
 	return
