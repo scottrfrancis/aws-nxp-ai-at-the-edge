02ec5250747bb7ee9bc6d4f02657781dc3a3e05b
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-09-13 15:38:55 -0300

    lambda_system_control: add board serial to MQTT topics
    Update MQTT topics to the format <system>/<board serial>/<data/control>
    to make it possible to filter data for specific device.

--- lambda_system_control/main.py
+++ lambda_system_control/main.py
@@ -35,18 +35,29 @@ client = greengrasssdk.client('iot-data')
 
 # Update values on device shadow changes!
 def function_handler(event, context):
-	if context.client_context.custom['subject'] == "$aws/things/" + core_name + "/shadow/update/delta":
-		for key,item in rest.items():
-			try:
-				item["value"] = event["current"]["state"]["desired"][key.split("/")[0]][key.split("/")[1]]
-				res = requests.get(item["url"] + item["value"])
-			except requests.exceptions.ConnectionError:
-				print("Connection error on REST endpoint " + item["url"] + ", retry on next loop!")
-			except Exception as e:
-				print("Unknown exception on REST endpoint " + item["url"] + ": " + repr(e))
-				print("Response error: " + str(res))
-	elif context.client_context.custom['subject'] == "cb/speed":
-		requests.get(rest["cb/speed"]["url"] + str(event["speed"]))
-	elif context.client_context.custom['subject'] == "led/brightness":
-		requests.get(rest["led/brightness"]["url"] + str(event["brightness"]))
+	print("Got topic " + context.client_context.custom['subject'] + ". Dumping event data:")
+	try:
+		boardSerial = str(requests.get("http://localhost:5001/info").json()["board-serial"])
+	except Exception as e:
+		print("Cannot get board serial, unable to handle your request: " + repr(e))
+	else:
+		if context.client_context.custom['subject'] == "$aws/things/" + core_name + "/shadow/update/delta":
+			for key,item in rest.items():
+				try:
+					item["value"] = event["current"]["state"]["desired"][key.split("/")[0]][key.split("/")[1]]
+					res = requests.get(item["url"] + item["value"])
+				except requests.exceptions.ConnectionError:
+					print("Connection error on REST endpoint " + item["url"] + ", retry on next loop!")
+				except Exception as e:
+					print("Unknown exception on REST endpoint " + item["url"] + ": " + repr(e))
+					print("Response error: " + str(res))
+		elif context.client_context.custom['subject'] == "cb/" + boardSerial + "/speed":
+			print("Updating cb " + boardSerial + " speed to: " + str(event["speed"]))
+			requests.get(rest["cb/speed"]["url"] + str(event["speed"]))
+		elif context.client_context.custom['subject'] == "led/" + boardSerial + "/brightness":
+			print("Updating led " + boardSerial + " brightness to: " + str(event["brightness"]))
+			requests.get(rest["led/brightness"]["url"] + str(event["brightness"]))
+		else:
+			print("No action on topic " + context.client_context.custom['subject'] + ". Dumping event data:")
+			print(str(event))
 	return
