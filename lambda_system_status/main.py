6cb5dc962661804935f83d692d6e0b19b1cb6569
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-09-13 15:36:54 -0300

    lambda_system_status: add board info to custom MQTT topic
    Use MQTT topics in format <system>/<board serial>/data, so it's possible
    to filter MQTT topics for a specific device.

--- lambda_system_status/main.py
+++ lambda_system_status/main.py
@@ -16,13 +16,14 @@ system_data_port = "5001"
 system_control_port = "5002"
 inference_info_port = "5003"
 rest = {
-	"cpu/data": "http://localhost:" + system_data_port + "/cpu",
-	"gpu/data": "http://localhost:" + system_data_port + "/gpu",
-	"ram/data": "http://localhost:" + system_data_port + "/ram",
-	#"cam/data": "http://localhost:" + system_data_port + "/cam",
-	"cb/data": "http://localhost:" + system_control_port + "/cb",
-	"led/data": "http://localhost:" + system_control_port + "/led",
-	"inference/data": "http://localhost:" + inference_info_port + "/inference"
+	"cpu/": "http://localhost:" + system_data_port + "/cpu",
+	"gpu/": "http://localhost:" + system_data_port + "/gpu",
+	"ram/": "http://localhost:" + system_data_port + "/ram",
+	#"cam/": "http://localhost:" + system_data_port + "/cam",
+	"info/": "http://localhost:" + system_data_port + "/info",
+	"cb/": "http://localhost:" + system_control_port + "/cb",
+	"led/": "http://localhost:" + system_control_port + "/led",
+	"inference/": "http://localhost:" + inference_info_port + "/inference"
 }
 
 # Creating a greengrass core sdk client
@@ -30,12 +31,18 @@ client = greengrasssdk.client('iot-data')
 
 # "THREAD" for MQTT connections
 def greengrass_mqtt_run():
+	boardSerial = 0
 	while True:
+		if not boardSerial:
+			try:
+				boardSerial = requests.get(rest["info/"]).json()["board-serial"]
+			except Exception as e:
+				print("Cannot get board serial: " + repr(e))
 		for topicMQTT,url in rest.items():
 			# Basically bridge REST to MQTT
 			try:
 				getData = requests.get(url)
-				client.publish(topic=topicMQTT, payload=getData)
+				client.publish(topic=topicMQTT + str(boardSerial) + "/data", payload=getData)
 				print("MQTT data published on " + topicMQTT)
 			except requests.exceptions.ConnectionError:
 				print("Connection error on topic " + topicMQTT + ", retry on next loop!")
