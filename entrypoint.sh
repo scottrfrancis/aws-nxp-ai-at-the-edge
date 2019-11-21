e56f85ee40c0a5b82b8cbbe05a9365ea86111cb4
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-11-21 10:51:03 -0300

    cloudformation: improve readability and logging
    Add more print statements to follow-up on the progress. Name variables
    read from command-line arguments for readability.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,14 @@
 PROGFILE=/progress.txt
 WEBDASHB=/webdashboards.txt
 AWSPROFILE=default
+UNIQUEHASH=$1
+GGNAME=$2
 AWSID=$3
 AWSSECRET=$4
 
 export PATH=$PATH:/root/.local/bin
+
+echo "Starting the provisioning of the AWS and Toradex AI at the Edge Demo"
 
 mkdir ~/.aws
 cd ~/.aws
@@ -32,6 +36,11 @@ cat appConfig.json | jshon -s $(echo $demoName) -i name > appConfig.json
 # Use default AWS CLI profile
 cat appConfig.json | jshon -s $(echo default) -i awsAdminProfile > appConfig.json
 
+echo "The unique prefix for this deployment is: $demoName"
+echo "The unique hash for this deployment is $UNIQUEHASH"
+echo "The string used for Greengrass name is: $GGNAME"
+echo "Running the web Dashboard Cloudformation. It will take some 20 minutes..."
+
 echo "10" > ${PROGFILE}
 
 yarn deploy
@@ -44,12 +53,13 @@ CFR=$(aws cloudfront list-distributions --no-paginate)
 echo $CFR | jshon -e DistributionList -e Items -a -e DomainName -u -p \
 	-e DefaultCacheBehavior -e TargetOriginId -u | grep -B 1 pastademo | grep \
 	-vE 'pastademo|^--$' >> ${WEBDASHB}
+echo "Web dashboard available at: $(cat ${WEBDASHB})"
 
 # run the script
 cd /aws-nxp-ai-at-the-edge
 
 # update the core shadow lambda with Cloudformation specific variable
-sed -i "s/coreplaceholder/${2}_Core/g" lambda_coreshadow/main.py
+sed -i "s/coreplaceholder/${GGNAME}_Core/g" lambda_coreshadow/main.py
 
 # update the dynamo table name with Cloudformation specific variable
 tableName=$(aws dynamodb list-tables --output=text | grep -o "\w*${demoName}.*")
@@ -57,4 +67,4 @@ sed -i "s/PastaDemoPlaceholder/${tableName}/g" lambda_dynamodb/main.py
 echo "65" > ${PROGFILE}
 
 # GG big bang
-./bigbang.sh $1 $2
+./bigbang.sh $UNIQUEHASH $GGNAME
